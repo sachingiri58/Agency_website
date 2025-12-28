69c945f2097bc116ded7ccb98a577107a95e8a98
sachin giri <sachin@Sachin>
2025-12-29 00:15:19 +0530

    Update Navbar Files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,57 @@
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets"; 
 
-const Navbar=({theme, setTheme})=>{
+const Navbar = ({ theme, setTheme }) => {
 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const [sidebarOpen, setSidebarOpen]=useState(false)
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop:-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
+    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
 
-<img src={theme=== 'dark' ? assets.logo_dark :assets.logo } className="w-32 sm:w-40" alt=''/>
+      <img
+        src={theme === "dark" ? assets.logo_dark : assets.logo}
+        className="w-32 sm:w-40"
+        alt=""
+      />
 
+      <div
+        className={`text-gray-700 dark:text-white sm:text-sm ${
+          !sidebarOpen ? "max-sm:w-0" : "max-sm:w-60 max-sm:pl-10"
+        } max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}
+      >
 
-<div className={`text-gray-700 dark:text-white sm:tect-sm ${!sidebarOpen ? 'max-sm:w-0' : 'max-sm:w-60 max-sm:pl-10' }max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
+        <img
+          src={assets.close_icon}
+          alt=""
+          className="w-5 absolute right-4 top-4 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
 
-<img src={assets.close_icon}  alt=""  className="w-5 absolute right-4 top-4 sm:hidden" onClick={()=> setSidebarOpen(false)}/>
+        <a onClick={() => setSidebarOpen(false)} href="#" className="sm:hover:border-b">Home</a>
+        <a onClick={() => setSidebarOpen(false)} href="#services" className="sm:hover:border-b">Service</a>
+        <a onClick={() => setSidebarOpen(false)} href="#our-work" className="sm:hover:border-b">Our Work</a>
+        <a onClick={() => setSidebarOpen(false)} href="#contact-us" className="sm:hover:border-b">Contact Us</a>
+      </div>
 
+      <div className="flex items-center gap-2 sm:gap-4">
+        <img
+          src={assets.close_icon}
+          alt=""
+          className="w-8 sm:hidden absolute right-4 top-4"
+          onClick={() => setSidebarOpen(true)}
+        />
 
+        <a
+          href=""
+          className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all"
+        >
+          Connect
+          <img src={assets.arrow_icon} width={14} alt="" />
+        </a>
+      </div>
 
-  <a href="#" className="sm:hover:border-b">Home</a><a href="#services" className="sm:hover:border-b">Service</a>
-  <a href="#our-work" className="sm:hover:border-b">Our Work</a>
-  <a href="#contact-us" className="sm:hover:border-b">Contact Us</a>
-</div>
-
-
-<a href="" className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all">Connect
- <img src={assets.arrow_icon} width={14} alt=""/></a>
     </div>
-  )
+  );
+};
 
-}
-export default Navbar
+export default Navbar;
