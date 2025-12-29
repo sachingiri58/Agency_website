55739c8ec7e5a1b69ab419b3619417624f89e3ff
sachin giri <sachin@Sachin>
2025-12-29 22:43:06 +0530

    Frontend changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar"; 
+import Hero from "./components/Hero";
+import Trusted from "./components/TrustedBy";
 const App =()=>{
 
 
@@ -9,6 +11,8 @@ const App =()=>{
   
       <div className="dark:bg-black relative">
         <Navbar theme={theme} setTheme={setTheme}/>
+        <Hero/>
+        <Trusted/>
       </div>
     
   )
